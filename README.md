6d2cf30b8dd5cc944624d246af7f25f7b383ae4e
DQ-Li <1372728616@qq.com>
2021-08-06 11:09:04 +0800

    修改baseURL

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# xmgmail
+# DQ-mail
 
 ## Project setup
 ```
--- src/network/axios.js
+++ src/network/axios.js
@@ -4,7 +4,7 @@ export default function axios (option) {
   return new Promise((resolve, reject) => {
     // 1.创建axios的实例
     const instance = originAxios.create({
-      baseURL: 'http://152.136.185.210:7878/api/m5',
+      baseURL: 'http://123.207.32.32:8000',
       timeout: 5000
     })
 
